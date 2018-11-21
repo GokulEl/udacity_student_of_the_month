6bd2b2a10baa8620b6b7bdb0af5cfc9bf84fcbaf
Gokul Elumalai <40486035+GokulEl@users.noreply.github.com>
2018-11-21 23:43:21 +0530

    Update quiz_1.py
    Updated

--- quiz_1.py
+++ quiz_1.py
@@ -1,13 +1,11 @@
 def remove_capital(sentence):
     result = ''
     words = list(sentence.split())
-    print(words)
+
     # Remove the word with the Capital letter
     for word in words[-1:0:-1]:
         if word[0] == word[0].upper():
-            print("yes")
             words.remove(word)
-            print(word)
             
     # Append the words together
     for word in words:
